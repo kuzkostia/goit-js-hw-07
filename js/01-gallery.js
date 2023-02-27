f8749938ca6f97bcdc23b7d7513f31972771f241
kuzkostia <112246522+kuzkostia@users.noreply.github.com>
2023-02-27 12:27:27 +0200

    Summary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+//! Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const images = createImageGallery(galleryItems);
+
+function createImageGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return (
+        `<div class="gallery__item">
+          <a class="gallery__link" href=${original}>
+            <img
+              class="gallery__image"
+              src=${preview}
+              data-source=${original}
+              alt=${description}
+            />
+          </a>
+        </div>`
+      );
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", images);
+galleryContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+});
